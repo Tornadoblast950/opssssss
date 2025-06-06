868243e5676525a8c1dab8d32d3648bb5dbc601e
Tornadoblast950 <jonas.rouca@gmail.com>
2025-06-06 09:12:07 +0200

    comments

--- Untitled-1.bat
+++ Untitled-1.bat
@@ -1,10 +1,13 @@
 @echo off
+:: 3 inputs for the user \/
 set /p pom1=Input number
 set /p pom2=Input number
 set /p pom3=Input what operation you want 1 for plus 2 for multiplication 3 for divison
+::using numbers 'cause strings are hard /\
 set /a pom4=%pom1%+%pom2%
 set /a pom5=%pom1%*%pom2%
 set /a pom6=%pom1%/%pom2%
+:: calculations /\
 if %pom3%== 1 (
   echo %pom4%
 )
@@ -14,4 +17,5 @@ if %pom3%== 3 (
 if %pom3%== 2 (
   echo %pom5%
 )
+:: inputs /\
 pause
